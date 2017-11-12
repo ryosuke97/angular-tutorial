d0cfa6bd082e169ed9d918587ae31f00a24bc02a
Ryosuke Yamano <ryosuke.y.97@gmail.com>
2017-11-12 11:32:36 +0900

    feature: ヒーローを配列から複数表示する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,24 @@ import { Hero } from './hero';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  heroes = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    name: 'superman',
-  };
+  selectedHero: Hero;
+
+  onSelect(hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
