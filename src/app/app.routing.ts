63b9bf5b1f37ed4c9b3ee3274bbb3cc751c9c51b
Ryosuke Yamano <ryosuke.y.97@gmail.com>
2017-11-13 23:53:47 +0900

    feature: ダッシュボード画面をルーティングで遷移できる

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,12 +2,22 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const appRoute: Routes = [
     {
         path: 'heroes',
         component: HeroesComponent
-    }
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoute);
